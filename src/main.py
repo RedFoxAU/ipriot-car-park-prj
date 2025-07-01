973f8b58eed91139bed14a7a86ce10d7a160f977
20031458 <20012345@users.noreply.github.com>
2025-07-01 17:19:47 +0800

    feat(core) main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
 from car_park import CarPark
-from sensor import Sensor
+from sensor import EntrySensor, ExitSensor
 from display import Display
 
 def main():
+# TODO: create a car park object with the location moondalup, capacity 100, and log_file "moondalup.txt"
+
+
+
+# TODO: Write the car park configuration to a file called "moondalup_config.json"
+
+
+# TODO: Reinitialize the car park object from the "moondalup_config.json" file
+
+
+# TODO: create an entry sensor object with id 1, is_active True, and car_park car_park
+
+
+# TODO: create an exit sensor object with id 2, is_active True, and car_park car_park
+
+
+# TODO: create a display object with id 1, message "Welcome to Moondalup", is_on True, and car_park car_park
+
+
+# TODO: drive 10 cars into the car park (must be triggered via the sensor - NOT by calling car_park.add_car directly)
+
+
+# TODO: drive 2 cars out of the car park (must be triggered via the sensor - NOT by calling car_park.remove_car directly)
+
+
+
     # Create display
     display1 = Display(id=1, message="Welcome to Moondalup Car Park", is_on=True)
 
