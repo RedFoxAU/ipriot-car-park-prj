e72cd3376e80d8b433bcd53fd87a3b592c781a7b
20031458 <20012345@users.noreply.github.com>
2025-07-01 13:45:22 +0800

    Added unit tests for the car park class

--- src/sensor.py
+++ src/sensor.py
@@ -1,66 +1,32 @@
-class Sensor:
-    def __init__(self, sensor_id, bay_id, occupied=False):
-        self.sensor_id = sensor_id
-        self.bay_id = bay_id
-        self.occupied = occupied
-
-    def __str__(self):
-        status = "Occupied" if self.occupied else "Available"
-        return f"Sensor {self.sensor_id} monitoring bay {self.bay_id}: {status}"
-
-    def detect_car_entry(self, license_plate):
-        pass
-
-    def detect_car_exit(self):
-        pass
-
-
-# class Sensor:
-#     def __init__(self, id ...): # Add the other parameters
-#         self.id = id
-#         self.is_active = is_active
-#         ... # Add the other attributes
-
-#     def __str__(self):
-#         ... # Return a string containing the sensor's id and status
-
-# class EntrySensor(Sensor):
-#     ...
-
-# # Also create an ExitSensor class
-
-class Sensor:
-    def __init__(self, car_park):
+from abc import ABC, abstractmethod
+import random
+class Sensor(ABC):
+    def __init__(self, car_park, sensor_id, is_active):
         self.car_park = car_park
+        self.id = sensor_id
+        self.is_active = is_active
+        self.car_park.add_sensor(self)
 
-    def detect_car(self):
-        pass  # To be implemented in subclasses
-
+    @abstractmethod
     def update_car_park(self, plate):
-        pass  # To be implemented in subclasses
+        pass
 
+    def _scan_plate(self):
+      return 'FAKE-' + format(random.randint(0, 999), "03d")
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
 
 class EntrySensor(Sensor):
-    def detect_car(self):
-        # Simulate detecting a car entering
-        plate = self.scan_plate()
-        self.update_car_park(plate)
-
-    def scan_plate(self):
-        # Simulated plate detection
-        return "ABC123"
-
     def update_car_park(self, plate):
         self.car_park.add_car(plate)
-
+        print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
-    def detect_car(self):
-        plate = self.scan_plate()
-        self.update_car_park(plate)
-
-    def scan_plate(self):
-        return "ABC123"
-
     def update_car_park(self, plate):
-        self.car_park.remove_car(plate)
+      self.car_park.remove_car(plate)
+      print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
