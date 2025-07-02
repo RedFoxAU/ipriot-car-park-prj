033b6768bdc245d08307f1ba3c399d60d85141f4
20031458 <20012345@users.noreply.github.com>
2025-07-02 08:55:14 +0800

    Added unit tests for logging car activity

--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -17,7 +17,7 @@ class TestCarPark(unittest.TestCase):
         self.assertEqual(self.car_park.plates, [])
         self.assertEqual(self.car_park.displays, [])
         self.assertEqual(self.car_park.available_bays, 100)
-        self.assertEqual(self.car_park.log_file, self.log_path)
+        self.assertEqual(self.car_park.log_file, Path("log.txt"))
 
     def test_add_car(self):
         self.car_park.add_car("FAKE-001")
@@ -48,7 +48,8 @@ class TestCarPark(unittest.TestCase):
             self.car_park.register("Not a Sensor or Display")
 
     def test_log_file_created(self):
-        self.assertTrue(self.log_path.exists())
+        new_carpark = CarPark("123 Example Street", 100, log_file = "new_log.txt")
+        self.assertTrue(Path("new_log.txt").exists())
 
     def test_car_logged_when_entering(self):
         self.car_park.add_car("NEW-001")
@@ -59,13 +60,14 @@ class TestCarPark(unittest.TestCase):
         self.assertIn("\n", last_line)
 
     def test_car_logged_when_exiting(self):
+        new_carpark = CarPark("123 Example Street", 100, log_file = "new_log.txt") # TODO: change this to use a class attribute or new instance variable
         self.car_park.add_car("NEW-001")
         self.car_park.remove_car("NEW-001")
         with self.car_park.log_file.open() as f:
             last_line = f.readlines()[-1]
-        self.assertIn("NEW-001", last_line)
-        self.assertIn("exited", last_line)
-        self.assertIn("\n", last_line)
+        self.assertIn(last_line, "NEW-001")
+        self.assertIn(last_line, "exited")
+        self.assertIn(last_line, "\n")
 
 if __name__ == "__main__":
     unittest.main()
