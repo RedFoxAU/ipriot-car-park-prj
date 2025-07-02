94be11330be689071392f11dc7a50ff383be722e
20031458 <20012345@users.noreply.github.com>
2025-07-02 11:39:42 +0800

    fix(main) main.py optimisation

--- src/main.py
+++ src/main.py
@@ -3,29 +3,28 @@ from sensor import EntrySensor, ExitSensor
 from display import Display
 
 def main():
-# TODO: create a car park object with the location moondalup, capacity 100, and log_file "moondalup.txt"
+    # TODO: create a car park object with the location moondalup, capacity 100, and log_file "moondalup.txt"
+    car_park = CarPark(location="Moondalup", capacity=100, log_file=Path("moondalup.txt"))
+
+    # TODO: Write the car park configuration to a file called "moondalup_config.json"
+    car_park.write_config("moondalup_config.json")
+
+    # TODO: Reinitialize the car park object from the "moondalup_config.json" file
 
 
-
-# TODO: Write the car park configuration to a file called "moondalup_config.json"
-
-
-# TODO: Reinitialize the car park object from the "moondalup_config.json" file
+    # TODO: create an entry sensor object with id 1, is_active True, and car_park car_park
 
 
-# TODO: create an entry sensor object with id 1, is_active True, and car_park car_park
+    # TODO: create an exit sensor object with id 2, is_active True, and car_park car_park
 
 
-# TODO: create an exit sensor object with id 2, is_active True, and car_park car_park
+    # TODO: create a display object with id 1, message "Welcome to Moondalup", is_on True, and car_park car_park
 
 
-# TODO: create a display object with id 1, message "Welcome to Moondalup", is_on True, and car_park car_park
+    # TODO: drive 10 cars into the car park (must be triggered via the sensor - NOT by calling car_park.add_car directly)
 
 
-# TODO: drive 10 cars into the car park (must be triggered via the sensor - NOT by calling car_park.add_car directly)
-
-
-# TODO: drive 2 cars out of the car park (must be triggered via the sensor - NOT by calling car_park.remove_car directly)
+    # TODO: drive 2 cars out of the car park (must be triggered via the sensor - NOT by calling car_park.remove_car directly)
 
 
 
